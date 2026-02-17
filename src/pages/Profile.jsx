5f4fb482bfaa04856b3d269ce8169fdfa285996c
Alexander E <alexander.egorov12@gmail.com>
2026-02-17 15:52:50 +0000

    File changes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -108,7 +108,7 @@ export default function Profile() {
           <h1 className={`text-xl font-bold ${theme === 'light' ? 'text-gray-900' : 'text-white'}`}>Профиль</h1>
           <Link to={createPageUrl('DailyTracker')}>
             <Button variant="ghost" size="icon" className={`h-[60px] w-[60px] rounded-full ${theme === 'light' ? 'hover:bg-black/5' : 'hover:bg-white/10'}`}>
-            <X style={{ width: 15, height: 15 }} />
+            <X style={{ width: 25, height: 25 }} />
             </Button>
           </Link>
         </div>
