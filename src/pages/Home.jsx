e9c1848be2d043d21566cfa094ff109802fc8ae2
Alexander E <alexander.egorov12@gmail.com>
2026-03-20 11:19:47 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { createPageUrl } from '@/utils';
 
 export default function Home() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    navigate(createPageUrl('DailyTracker'), { replace: true });
+    navigate('/DailyTracker', { replace: true });
   }, [navigate]);
 
   return (
